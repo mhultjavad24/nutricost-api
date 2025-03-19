718c8672a7c4490fad91e9d316aa7d0136ff06d9
Marcus Hultgren <marcus.hultgren@yh.nackademin.se>
2025-03-19 22:54:29 +0100

    Add echo endpoint

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "Hello World!"
+
+@app.route("/echo")
+def echo():
+    message = request.args.get("message", "")
+    return {"message": message}
+
+if __name__ == "__main__":
+    app.run(debug=True)
